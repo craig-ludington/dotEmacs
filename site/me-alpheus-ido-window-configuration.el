b5912751638c9e07badee285989058a0f1b31c51
Craig Ludington <me@alpheus.me>
2013-11-05 12:16:12 -0600

    Use high registers (> 32768) to save window configurations in site/me-alpheus-ido-window-configuration.el

--- site/me-alpheus-ido-window-configuration.el
+++ site/me-alpheus-ido-window-configuration.el
@@ -4,35 +4,24 @@
 
 (require 'ido)
 
-(defvar *me.alpheus/window/register-map*
+(defvar *me.alpheus/window/register-map* 
   '()
-  "Map window configuration names to the register they're stored in.  E.g. '((\"Work\" . ?) (\"Play\" . ?p)")
+  "Map window configuration names to the register they're stored in.  E.g. '((\"Work\" . 32768) (\"Play\" . 32769)")
 
-(defun me.alpheus/window/register-of (name)
-  (cdr (assoc name *me.alpheus/window/register-map*)))
-
-(defun me.alpheus/window/add-mapping (name register)
-  (add-to-list '*me.alpheus/window/register-map* (cons name register)))
-
-(defun me.alpheus/window/save (register)
-  (interactive "cWindow configuration register? ")
-  (if (and (get-register register)
-	   (not (y-or-n-p (format "Register '%c' is in use.  Override?" register))))
-      (message "Skipping")
-    (let ((name (read-from-minibuffer "Window configuration name? ")))
-      (if (and (me.alpheus/window/register-of name)
-	       (not (y-or-n-p (format "\"%s\" is in use.  Override?" name))))
-	  (message "Skipping")
-	(progn (window-configuration-to-register register)
-	       (me.alpheus/window/add-mapping name register))))))
+(defun me.alpheus/window/save (name)
+  (interactive "MSave window configuration as: ")
+  (let ((register (loop for c from 32768 unless (get-register c) return c)))
+    (flet ((save (name) 
+		 (window-configuration-to-register register)
+		 (add-to-list '*me.alpheus/window/register-map* (cons name register))))
+      (if (assoc name *me.alpheus/window/register-map*)
+	  (when (y-or-n-p (format "\"%s\" is in use.  Override?" name))
+	    (save name))
+	(save name)))))
 
 (defun me.alpheus/window/restore ()
   "Prompt for a window configuration to restore, the IDO way."
   (interactive)
-  (jump-to-register (me.alpheus/window/register-of 
-		     (ido-completing-read "Window configuration: "
-					  (mapcar 'car *me.alpheus/window/register-map*)))))
-
-
-;;; window-configuration-p
-;;; register-alist
+  (let* ((name     (ido-completing-read "Switch to: " (mapcar 'car *me.alpheus/window/register-map*)))
+	 (register (cdr (assoc name *me.alpheus/window/register-map*)) ))
+    (jump-to-register register)))
